cb8ae4ec83f0c60e95ff332bf47a50a8a9dd9067
rabbit0057 <dilip.0057@gmail.com>
2018-06-11 00:43:09 +0530

    Add error handler for PATH "/" and "/error"

--- src/main/java/com/assignment/qa/controller/CustomerController.java
+++ src/main/java/com/assignment/qa/controller/CustomerController.java
@@ -3,19 +3,33 @@ package com.assignment.qa.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.web.ErrorController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
 
 import com.assignment.qa.model.Customer;
 import com.assignment.qa.service.CustomerService;
 
 @RestController
-public class CustomerController {
+public class CustomerController implements ErrorController {
 
 	@Autowired
 	private CustomerService customerService;
-
+	
+	@RequestMapping("/error")
+	public String handleError() {
+		//do something like logging
+		return "404 PAGE NOT FOUND";
+	}
+	@RequestMapping("/")
+	public String welcome() {
+		//do something like logging
+		return "WELCOME TO CUSTOMER ONBOARDING ";
+	}
+	
+	
 	@RequestMapping("/create")
 	public String create(@RequestParam String firstName,@RequestParam String lastName, @RequestParam long mobileNumber, @RequestParam String location) {
 		Customer c = customerService.create(firstName, lastName, mobileNumber, location);
@@ -40,6 +54,11 @@ public class CustomerController {
 		customerService.deleteAll();
 		return "Deleted All records";
 		
+	}
+	@Override
+	public String getErrorPath() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
