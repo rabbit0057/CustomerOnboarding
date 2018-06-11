c9fe949b95d26317b14b363c8cfd83b521783b7a
rabbit0057 <dilip.0057@gmail.com>
2018-06-11 14:26:16 +0530

    Fixed deployment issue in AWS elastic bean EC2

--- src/main/java/com/assignment/qa/CustomerOnboardingApplication.java
+++ src/main/java/com/assignment/qa/CustomerOnboardingApplication.java
@@ -2,9 +2,10 @@ package com.assignment.qa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CustomerOnboardingApplication {
+public class CustomerOnboardingApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CustomerOnboardingApplication.class, args);
