f6d71dbc7584248c4439d835879201c1ec30b887
rabbit0057 <dilip.0057@gmail.com>
2018-06-11 11:59:41 +0530

    Adding quotes

--- src/main/java/com/assignment/qa/controller/CustomerController.java
+++ src/main/java/com/assignment/qa/controller/CustomerController.java
@@ -27,27 +27,35 @@ public class CustomerController implements ErrorController {
 		//do something like logging
 		return "WELCOME TO CUSTOMER ONBOARDING ";
 	}
-	        
+	
+	// *********************Onboarding New Customer **********************        
 	@RequestMapping(value = "/newCustomer")
 	public String create(@RequestParam String firstName,@RequestParam String lastName, @RequestParam long mobileNumber, @RequestParam String location) {
 		Customer c = customerService.create(firstName, lastName, mobileNumber, location);
 		return c.toString();
 		
 	}
+	// ********************* Show all Customer **********************        
 	@RequestMapping("/getAllCustomer")
 	public List<Customer>getAll(){
 		return customerService.getAll();
 	}
+	// *********************Update Customer **********************        
 	@RequestMapping("/UpdateCustomer")
 	public String update(@RequestParam String firstName,@RequestParam String lastName, @RequestParam long mobileNumber, @RequestParam String location ) {
 		Customer c = customerService.Update(firstName, lastName, mobileNumber, location);
 		return c.toString();
 	}
+	// *********************Delete A Customer **********************        
+
 	@RequestMapping("/deleteCustomer")
 	public String delete(@RequestParam String firstName) {
 		customerService.delete(firstName);
 		return "Deleted "+firstName;
 	}
+	
+	// *********************Delete All Customer **********************        
+
 	@RequestMapping("/deleteAllCustomer")
 	public String deleteAll() {
 		customerService.deleteAll();
